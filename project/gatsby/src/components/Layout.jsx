db801d40fdb7999007b70d0e44febc07644c2cf3
Justin Walsh <contact.me@thejustinwalsh.com>
2020-12-28 12:59:08 -0700

    feature: Module 5

--- project/gatsby/src/components/Layout.jsx
+++ project/gatsby/src/components/Layout.jsx
@@ -33,7 +33,6 @@ export default function Layout({ children }) {
     <>
       <GlobalStyles />
       <Typography />
-
       <SiteBorderStyles>
         <ContentStyles>
           <Nav />
--- project/gatsby/src/pages/pizzas.jsx
+++ project/gatsby/src/pages/pizzas.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+
   return (
     <>
-      <h1>Pizzas</h1>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
